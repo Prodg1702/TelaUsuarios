591b01d77fa40be74fe126ee56014e06519a5076
Prodg1702 <106077622+Prodg1702@users.noreply.github.com>
2023-03-14 11:15:17 -0300

    Update teste.js

--- TA02/teste.js
+++ TA02/teste.js
@@ -1,23 +1,10 @@
-// const botao = document.querySelector('.botao-girar');
-
-// botao.addEventListener('click', () => {
-//     botao.classList.toggle('girando');
-// });
+// function toggleButton(button) {
+//   button.classList.toggle("selecionado");
+// }
 
 
-const botao1 = document.getElementById("botao1");
-const botao2 = document.getElementById("botao2");
+var botaoToggle = document.querySelector('.botao-toggle');
 
-botao1.addEventListener("click", () => {
-  botao2.disabled = true;
-});
-
-botao2.addEventListener("click", () => {
-  botao1.disabled = true;
-});
-
-
-const cell = document.querySelector('.selectable-cell');
-cell.addEventListener('click', () => {
-  alert('Célula selecionada!');
+botaoToggle.addEventListener('click', function() {
+  botaoToggle.classList.toggle('ativo');
 });
